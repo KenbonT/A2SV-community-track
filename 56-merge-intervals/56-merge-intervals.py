1d97b31ce5ae9f3c27205ea9d4c31db8bc894fa1
KenbonT <94214002+KenbonT@users.noreply.github.com>
2022-05-13 23:38:44 +0300

    Time: 149 ms (89.96%), Space: 18 MB (85.15%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,29 +1,29 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # intervals.sort()
-        # answer=[]
-        # pushedAnswer=intervals[0]
-        # index=0
-        # for i in intervals:
-        #     if(i[0]<=pushedAnswer[1]):
-        #         pushedAnswer[1]=max(pushedAnswer[1],i[1])
-        #     else:
-        #         answer.append(pushedAnswer)
-        #         pushedAnswer=i;
-        # answer.append(pushedAnswer)
-        # return answer;    
         intervals.sort()
         answer=[]
         pushedAnswer=intervals[0]
         index=0
-        for i in range(1,len(intervals)):
-            if(intervals[i][0]<=pushedAnswer[1]):
-                pushedAnswer[1]=max(pushedAnswer[1],intervals[i][1])
+        for i in intervals:
+            if(i[0]<=pushedAnswer[1]):
+                pushedAnswer[1]=max(pushedAnswer[1],i[1])
             else:
                 answer.append(pushedAnswer)
-                pushedAnswer=intervals[i];
+                pushedAnswer=i;
         answer.append(pushedAnswer)
-        return answer; 
+        return answer;    
+#         intervals.sort()
+#         answer=[]
+#         pushedAnswer=intervals[0]
+#         index=0
+#         for i in range(1,len(intervals)):
+#             if(intervals[i][0]<=pushedAnswer[1]):
+#                 pushedAnswer[1]=max(pushedAnswer[1],intervals[i][1])
+#             else:
+#                 answer.append(pushedAnswer)
+#                 pushedAnswer=intervals[i];
+#         answer.append(pushedAnswer)
+#         return answer; 
         
         
         
